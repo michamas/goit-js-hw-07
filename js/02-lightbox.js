e906f8e9911ec26e4c22c182036c0a226bf01b43
Michał Masiulanis <m.masiulanis@gmail.com>
2022-12-19 22:16:28 +0100

    zad 2 works?

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,8 +8,6 @@ const gallery = document.querySelector(".gallery");
 // create gallery of divs with imgs from galleryItems
 const createGallery = () => {
   for (let i = 0; i < galleryItems.length; i++) {
-    let galleryDiv = document.createElement("div");
-
     let galleryLink = document.createElement("a");
     galleryLink.classList.add("gallery__item");
     galleryLink.href = galleryItems[i].original;
@@ -19,8 +17,7 @@ const createGallery = () => {
     galleryImg.src = galleryItems[i].preview;
     galleryImg.alt = galleryItems[i].description;
 
-    gallery.append(galleryDiv);
-    galleryDiv.append(galleryLink);
+    gallery.append(galleryLink);
     galleryLink.append(galleryImg);
   }
   console.log("I've created a gallery of small imgs");
@@ -28,27 +25,8 @@ const createGallery = () => {
 //execute function
 createGallery();
 
-//handling click - works if you click only on a picture
-// creates img by basicLightbox function "create"
+// point to SimpleLightbox which elements we want to handle
+const lightbox = new SimpleLightbox(".gallery .gallery__item");
+lightbox.on("enableKeyboard.simplelightbox", function () {});
 
-function clickImg(e) {
-  //   if (e.target.nodeName !== "IMG") {
-  //     return;
-  //   }
-  const instance = new SimpleLightbox("img");
-  // `<img src = ${e.target.dataset.source}>`
-
-  instance.show();
-  console.log(e.target.alt);
-}
-
-// document.addEventListener("keydown", function closeImg(event) {
-//   if (event.key === "Escape") {
-//     instance.close();
-//     console.log("pressed ESC");
-//     document.removeEventListener("keydown", closeImg);
-//   }
-// });
-
-gallery.addEventListener("click", clickImg);
 console.log(galleryItems);
